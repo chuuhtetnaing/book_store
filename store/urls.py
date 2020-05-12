62212fffba0e93ed8c537f75ec869552c13eea31
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2020-05-13 02:42:09 +0800

    cart works and commit for gcp

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("home", views.home, name="home"),
+    path("view_cart", views.view_cart, name="cart_page"),
+    path("add_to_cart/<int:book_id>", views.add_to_cart, name="add_to_cart"),
+    path("remove_from_cart/<int:book_id>", views.remove_from_cart, name="remove_from_cart"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 import csv, io
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from store.models import Book
 
@@ -7,5 +7,33 @@ def home(request):
     books = Book.objects.all()[:30]
     for book in books:
         book.description = book.description[:100] + " ............"
-    context = {"books": books}
+    cart = request.session.get('cart')
+    context = {"books": books, "cart": cart}
     return render(request, 'store/home.html', context)
+
+def view_cart(request):
+    cart = request.session.get('cart')
+    orders = list()
+    if not cart:
+        return render(request, 'store/cart.html', {"cart": cart}) 
+    for book_id in cart:
+        orders.append(Book.objects.get(book_id=book_id))
+    context = {"cart": orders}
+    return render(request, 'store/cart.html', context)
+
+def add_to_cart(request, book_id):
+    context = {}
+    if request.session.get('cart') is None:
+        request.session['cart'] = []
+    # return render(request, 'store/cart.html', context)
+    orders = request.session.get('cart')
+    orders.append(str(book_id))
+    request.session['cart'] = orders
+    return redirect('home')
+
+def remove_from_cart(request, book_id):
+    context = {}
+    orders = request.session.get('cart')
+    orders.remove(str(book_id))
+    request.session['cart'] = orders
+    return redirect('cart_page')
