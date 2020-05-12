226a81a10a0016fff5b7c90e3e8fe8211d96eee5
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2020-05-12 17:42:01 +0800

    first try to heroku

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'store',
-    'csvimport.app.CSVImportConf',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
+import csv, io
 from django.shortcuts import render
+from django.contrib import messages
+from store.models import Book
 
-# Create your views here.
+def home(request):
+    books = Book.objects.all()[:30]
+    for book in books:
+        book.description = book.description[:100] + " ............"
+    context = {"books": books}
+    return render(request, 'store/home.html', context)
