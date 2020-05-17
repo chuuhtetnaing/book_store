aaf5d9342938df63ac1c3bb0bf9115f612fc75f4
Chuu Htet Naing <chuuhtetnaing@gmail.com>
2020-05-18 06:57:04 +0800

    added item-to-item and frequent bought items

--- store/views.py
+++ store/views.py
@@ -6,8 +6,10 @@ from store.models import Book, Genre
 from django.core.paginator import Paginator
 from joblib import load
 from django.conf import settings
+from surprise import dump
 
-loaded_rules = load(os.path.join(settings.BASE_DIR, 'rules.sav'))
+mba_rules = load(os.path.join(settings.BASE_DIR, 'rules.sav'))
+_, cf_item_to_item = dump.load(os.path.join(settings.BASE_DIR, 'cf_item_to_item.sav'))
 
 def home(request):
     context = {}
@@ -44,11 +46,38 @@ def books(request, genre):
 def view_book(request, book_id):
     cart = request.session.get('cart')
     book = Book.objects.get(book_id=book_id)
-    recommendation_book_id = [list(x) for x in list(loaded_rules[(loaded_rules['antecedents'] == {book_id}) & (loaded_rules['consequents_len'] == 1)].sort_values(by=['lift'], ascending=False)['consequents'][:3])]
-    recommendation_books = list()
-    for book_id in recommendation_book_id:
-        recommendation_books.append(Book.objects.get(book_id=book_id[0]))
-    context = {"book": book, 'recommendation_books': recommendation_books, "cart": cart}
+    
+    itemset1_recommendation_book_id = [list(x) for x in list(mba_rules[(mba_rules['antecedents'] == {book_id}) & (mba_rules['consequents_len'] == 1)].sort_values(by=['lift'], ascending=False)['consequents'][:3])]
+    
+    if itemset1_recommendation_book_id:
+        itemset1_recommendation_books = list()
+        for id in itemset1_recommendation_book_id:
+            itemset1_recommendation_books.append(Book.objects.get(book_id=id[0]))
+    else:
+        itemset1_recommendation_books = None
+
+    check_len = [list(x) for x in list(mba_rules[(mba_rules['antecedents'] == {book_id}) & (mba_rules['consequents_len'] == 3)].sort_values(by=['lift'], ascending=False)['consequents'][:3])]
+    if len(check_len) == 3:
+        itemset3_recommendation_book_id = [list(x) for x in list(mba_rules[(mba_rules['antecedents'] == {book_id}) & (mba_rules['consequents_len'] == 3)].sort_values(by=['lift'], ascending=False)['consequents'][2:3])][0]
+    elif len(check_len) == 2:
+        itemset3_recommendation_book_id = [list(x) for x in list(mba_rules[(mba_rules['antecedents'] == {book_id}) & (mba_rules['consequents_len'] == 3)].sort_values(by=['lift'], ascending=False)['consequents'][1:2])][0]
+    elif len(check_len) == 1:
+        itemset3_recommendation_book_id = [list(x) for x in list(mba_rules[(mba_rules['antecedents'] == {book_id}) & (mba_rules['consequents_len'] == 3)].sort_values(by=['lift'], ascending=False)['consequents'][:1])][0]
+    else:
+        itemset3_recommendation_book_id = None
+
+    if itemset3_recommendation_book_id:
+        itemset3_recommendation_books = list()
+        for id in itemset3_recommendation_book_id:
+            itemset3_recommendation_books.append(Book.objects.get(book_id=id))
+    else:
+        itemset3_recommendation_books = None
+
+    book_inner_id = cf_item_to_item.trainset.to_inner_iid(book_id)
+    current_book_neighbors = cf_item_to_item.get_neighbors(book_inner_id, k=3)
+    current_book_neighbors = [Book.objects.get(book_id=cf_item_to_item.trainset.to_raw_iid(inner_id)) for inner_id in current_book_neighbors]
+
+    context = {"book": book, 'itemset1_recommendation_books': itemset1_recommendation_books, 'itemset3_recommendation_books': itemset3_recommendation_books, 'item_to_item_recommendation_books': current_book_neighbors, "cart": cart}
     return render(request, 'store/book_detail.html', context)
 
 def view_cart(request):
